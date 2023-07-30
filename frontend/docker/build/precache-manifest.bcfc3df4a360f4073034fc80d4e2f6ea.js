9121c20e9982cd1be256ab4d26732fa9e79da89e
raisack8 <raisack8@gmail.com>
2023-07-30 10:49:26 +0900

    0730 アイコン等基本上方修正

---
+++ frontend/docker/build/precache-manifest.bcfc3df4a360f4073034fc80d4e2f6ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "82b6438ed3b428110459",
+    "url": "/static/css/main.39ddfc56.chunk.css"
+  },
+  {
+    "revision": "82b6438ed3b428110459",
+    "url": "/static/js/main.609f729d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "81170d6877edbb0b9a3a",
+    "url": "/static/js/2.9d197bef.chunk.js"
+  },
+  {
+    "revision": "b24a6f53cb76a08bf8653f26d977077c",
+    "url": "/index.html"
+  }
+];
